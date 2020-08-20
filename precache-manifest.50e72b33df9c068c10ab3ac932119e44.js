ba67570973f745f850b08779990cfdecf962021e
unknown <ogbonnakezie@gmail.com>
2020-08-20 15:03:44 +0100

    Updates

---
+++ precache-manifest.50e72b33df9c068c10ab3ac932119e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8567e9b341152a0fe6a3b8de310c24",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "2bd586429c34c1230865",
+    "url": "/weather/static/css/main.3046b42d.chunk.css"
+  },
+  {
+    "revision": "8f0317f40f780cd29c86",
+    "url": "/weather/static/js/2.60a9cc99.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/weather/static/js/2.60a9cc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd586429c34c1230865",
+    "url": "/weather/static/js/main.46797afd.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
